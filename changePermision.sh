854df46eb9f6c42a62c1b7980a2ed022f7cc3a1b
BoniW <32690295+BoniWPy@users.noreply.github.com>
2019-04-17 14:23:40 +0700

    fix type string ""

--- changePermision.sh
+++ changePermision.sh
@@ -2,9 +2,9 @@
 echo -e "which folder will be changed the permision? : \n the format must like /etc/apache/ !!! "
 echo "masukan folder mana yang akan di rubah permision : "(mesti dalam format /etc/apache/)""
 read folder
-echo "masukan folder mana yang akan di rubah permision : "(mesti dalam format : 644)" "
+echo "masukan folder mana yang akan di rubah permision : (mesti dalam format : 644) "
 read folderPErmision
-echo "masukan folder mana yang akan di rubah permision : "(mesti dalam format : 775)" "
+echo "masukan folder mana yang akan di rubah permision : (mesti dalam format : 775) "
 read filePermision
 sudo find $folder -type d -exec chmod $folderPermision {} \;
 sudo find $folder -type f -exec chmod $filePermision {} \;

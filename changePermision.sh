e937413ad111ca8745f2765a3b0be1257ba585db
BoniW <32690295+BoniWPy@users.noreply.github.com>
2019-04-17 20:13:44 +0700

    Update changePermision.sh

--- changePermision.sh
+++ changePermision.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+echo -e "#==============================================================# "
+echo -e  "# Name:         Automation change permision on linux          #"
+echo -e  "# Description:  change persmision linux                       #"
+echo -e  "# Version:      V.Karya Anak Bangsa.1.1                       #"
+echo -e  "# Author     :  DevOps@blibli.com                             #"
+echo -e  "# License:      GNU General Public License, version 3 (GPLv3) #"
+echo -e  "# License URI:  http://www.gnu.org/licenses/gpl-3.0.html      #"
+echo -e  "#=============================================================#"
 echo -e "which folder will be changed the permision? : \n the format must like /etc/apache/ !!! "
 echo "masukan folder mana yang akan di rubah permision : "(mesti dalam format /etc/apache/)""
 read folder
-echo "masukan folder mana yang akan di rubah permision : (mesti dalam format : 644) "
+echo "masukan fol
+der mana yang akan di rubah permision : (mesti dalam format : 644) "
 read folderPErmision
 echo "masukan folder mana yang akan di rubah permision : (mesti dalam format : 775) "
 read filePermision

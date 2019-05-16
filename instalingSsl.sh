45912aa67219f04202bcc54a0f441109edf9efee
BoniW <32690295+BoniWPy@users.noreply.github.com>
2019-05-15 21:06:37 -0700

    add email input and domain names

--- instalingSsl.sh
+++ instalingSsl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo -e "#==============================================================# "
-echo -e  "# Name:         Script for put files into GCP Storage         #"
-echo -e  "# Description:  Install php5.6-fpm fast-cgi at ec2 instance   #"
+echo -e  "# Name:         automation installing ssl                     #"
+echo -e  "# Description:  Install SLL with FULL Automaticaly            #"
 echo -e  "# Version:      V.Karya Anak Bangsa.1.1                       #"
-echo -e  "# Author     :  DevOps@blibli.com                                     #"
+echo -e  "# Author     :  DevOps@payrightsystem.com                     #"
 echo -e  "# License:      GNU General Public License, version 3 (GPLv3) #"
 echo -e  "# License URI:  http://www.gnu.org/licenses/gpl-3.0.html      #"
 echo -e  "#=============================================================#"
@@ -11,4 +11,9 @@ echo -e  "#=============================================================#"
 sudo add-apt-repository ppa:certbot/certbot ;
 sudo apt-get update ;
 sudo apt-get install python-certbot-apache ;
+echo -e "Please Input Your Domain Name !!!"
+read domain
+echo -e "Please input your email address..!!!"
+read EMAIL
+sudo certbot run -d $domain -m $EMAIL --agree-tos
 

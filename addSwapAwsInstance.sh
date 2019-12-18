853b413d4669d164a68670af9255a75eb875c3a1
BoniW <32690295+BoniWPy@users.noreply.github.com>
2019-12-18 18:59:38 +0700

    Update addSwapAwsInstance.sh

--- addSwapAwsInstance.sh
+++ addSwapAwsInstance.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 echo "MAU DIBUAT SWAP BERAPA GIGA MAS BOYSSSS followed by [ENTER]:=> dalam bentuk 4000kb = 4GB"
-read size
+
 sudo fallocate -l $size /swapfile 
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile 
 sudo swapon /swapfile 
 
-/bin/dd if=/dev/zero of=/var/swap.1 bs=4M count=4028
+/bin/dd if=/dev/zero of=/var/swap.1 bs=4M count=1028
 /sbin/mkswap /var/swap.1
 /sbin/swapon /var/swap.1

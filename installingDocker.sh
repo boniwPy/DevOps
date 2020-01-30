932ea63baf51ce0e884cf19b829179e9f9549213
BoniW <32690295+BoniWPy@users.noreply.github.com>
2020-01-30 14:21:50 +0700

    Create installingDocker.sh

---
+++ installingDocker.sh
@@ -0,0 +1,6 @@
+apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update -y
+apt-get install docker-ce -y
+apt install sudo -y

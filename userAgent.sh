3ed22ecd1c15291f86b3244e3bd1aa50b6cdc988
BoniW <32690295+BoniWPy@users.noreply.github.com>
2020-05-13 23:23:01 +0700

    Create userAgent.sh

---
+++ userAgent.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/BoniWPy/DevOps/master/installingphp7-fpm.sh
+sudo bash installingphp7-fpm.sh
+wget https://raw.githubusercontent.com/BoniWPy/DevOps/master/installCodeDeploy.sh
+sudo bash installCodeDeploy.sh
+

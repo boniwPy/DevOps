04013a22cbb11ec757a5c326a74a9f2b44eca70d
BoniW <32690295+BoniWPy@users.noreply.github.com>
2020-05-13 23:33:53 +0700

    Update installCodeDeploy.sh

--- installCodeDeploy.sh
+++ installCodeDeploy.sh
@@ -3,7 +3,7 @@ sudo apt-get install ruby -y
 sudo apt-get install wget -y
 cd $HOME
 wget https://aws-codedeploy-ap-southeast-1.s3.ap-southeast-1.amazonaws.com/latest/install
-chmod +x ./install
+chmod +x install
 sudo ./install auto
 service=codedeploy-agent
 if pgrep -x "$service" >/dev/null  
